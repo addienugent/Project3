36a05bb1bccab36ab527b6d497900162a1ed3a10
addienugent <102487019+addienugent@users.noreply.github.com>
2022-07-10 16:17:50 -0400

    Create MainPage.xaml.cs

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using EcommerceAppMobile.ViewModels;
+using EcommerceAppMobile.Pages;
+using System.Diagnostics;
+
+namespace EcommerceAppMobile
+{
+    
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+           
+            InitializeComponent();
+    
+        }
+
+
+    }
+}
